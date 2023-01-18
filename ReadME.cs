de38bfcee6e26b71fd765aac2f841a1e564b6d22
david0347 <davidduh0347@gmail.com>
2023-01-18 11:20:36 -0500

    Added a ReadME

---
+++ ReadME.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * 
+ * Tournament Manager made by David Duh
+ * 
+ * This is the tournament manager I made for the project that you guys assigned
+ * There are two screens, one to update and manage gamer entries and another one to
+ * hold the tournament. It is a very basic tournament style with the players being placed
+ * by going down the list. If this was a real tournament manager I could add a random number
+ * generator to keep things interesting, but this program displays the data manipulating
+ * methods to alter a database by adding, deleting, and updating data.
+ * 
+ * If I was to do this project again more officialy there are some things I would do differently
+ * I would make the list re-compose the checkboxes every time one was deleted so it would be a
+ * smooth list. I would also add a random number generator to make the tournaments different and
+ * possibly make a dynamic number of people needed for a tournament(as long as it was an even number).
+ * I would also have created a class for the check boxes, I did not realize I would need them twice
+ * until I got to the next screen.
+ * 
+ * If you have any questions or want to see me implement anything else into the tournament manager, let
+ * me know. Thank you, David Duh.
+ */
